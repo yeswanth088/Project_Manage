aa36653c5ea37c5e20b74d91518acc03a937c916
Sravan94-git <sravansunkara04@gmail.com>
2026-02-27 15:44:02 +0530

    UI Updated

--- Project_Manage/Migrations/20260227083435_InitialCreate.Designer.cs
+++ Project_Manage/Migrations/20260227083435_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project_Manage.Models;
 
@@ -10,9 +11,11 @@ using Project_Manage.Models;
 namespace Project_Manage.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20260227083435_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
